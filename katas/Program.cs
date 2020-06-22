857ae9992b05e8f2a438c5b68b1e2f44670cd56b
Ebuka Mbata <rockymmpr@gmail.com>
2020-06-22 09:58:12 +0100

    first commit

---
+++ katas/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace katas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            katasolution solution = new katasolution();
+            //solution.kataSolution(400);
+            Console.WriteLine(solution.kataSolution(400));
+        }
+    }
+}
---
+++ katas/katasolution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace katas
+{
+    public class katasolution
+    {
+        public bool kataSolution(int num)
+        {
+            for (int i = 1; i*i <= num; i++)
+            {
+                if (i * i == num)
+                    return true;
+            }
+            return false;
+        }
+       
+    }
+}
